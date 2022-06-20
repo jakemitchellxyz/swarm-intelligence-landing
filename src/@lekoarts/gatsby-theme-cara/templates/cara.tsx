074719119ffc46b398beae7ffad1efe7eb648277
Jake Mitchell <jake@jakemitchell.xyz>
2022-06-20 02:06:31 -0500

    add teams

--- src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
+++ src/@lekoarts/gatsby-theme-cara/templates/cara.tsx
@@ -2,7 +2,7 @@ import * as React from "react"
 import { Parallax } from "@react-spring/parallax"
 import Layout from "../components/layout"
 import Hero from "../components/hero"
-// import Projects from "../components/projects"
+import Projects from "../components/projects"
 import About from "../components/about"
 import Contact from "../components/contact"
 
@@ -15,11 +15,11 @@ console.log(' servers - https://www.netlify.com/')
 
 const Cara = () => (
   <Layout>
-    <Parallax pages={3}>
+    <Parallax pages={4}>
       <Hero offset={0} factor={1} />
-      {/* <Projects offset={1} factor={2} /> */}
-      <About offset={1} factor={1} />
-      <Contact offset={2} factor={1} />
+      <Projects offset={1} factor={1} />
+      <About offset={2} factor={1} />
+      <Contact offset={3} factor={1} />
     </Parallax>
   </Layout>
 )
